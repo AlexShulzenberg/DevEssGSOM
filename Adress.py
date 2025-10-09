7ab2d6f1c17b8e1d7ebfea98f94d02581327176f
Arsen <st087432@student.spbu.ru>
2025-10-09 07:44:48 +0000

    1 file addded

---
+++ Adress.py
@@ -0,0 +1,7 @@
+# Get the address out of the string
+
+text = "Before departure, all students leaving for exchange studies should fill in a specialized application form. An application should be submitted in 1 copy and not later than 2 weeks prior to departure for the exchange semester, provided that the students do not have any academic failure. The paper should be submitted to the International Academic Mobility Office (Volkhovsky per., 3, office 207, Tel.: +7 (812) 323 8447). A copy of the invitation letter from a host school - partner of GSOM SPbU has to be attached to the application."
+adress_starts = text.find("(") + 1
+adress_ends = text.find(", Tel")
+uchatsya_umu_rasumu_tut = text[adress_starts:adress_ends]
+print(uchatsya_umu_rasumu_tut)
